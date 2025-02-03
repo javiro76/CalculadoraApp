c1e1cd145747312750d6e43e0d17dd3230f1cf9e
javiro76 <javiro76@gmail.com>
2025-02-02 21:10:00 -0500

    add menu

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -3,14 +3,53 @@ import java.util.Scanner;
 public class CalculadoraApp {
 
     public static void main(String[] args) {
-        System.out.println("**** Aplicación Calculadora ****");
-        System.out.print("Proporciona valor operando 1:");
         Scanner consola = new Scanner(System.in);
-        var operando1 = Integer.parseInt(consola.nextLine().trim());
-        System.out.print("Proporciona valor operando 2:");
-        var operando2 = Integer.parseInt(consola.nextLine().trim());
-        var resultado = operando1 + operando2;
-        System.out.println("Resultado: " + resultado);
+        System.out.println("**** Aplicación Calculadora ****");
+        System.out.println("""
+        Selecciona el tipo de operación:
+        1. Suma
+        2. Resta
+        3. Multiplicación
+        4. División
+        5. Salir
+        """);
+        System.out.print("Opción Seleccionada:");
+        var operacion = Integer.parseInt(consola.nextLine().trim());
+
+        int resultado;
+
+        if( operacion >= 1 && operacion <= 4 ){
+            System.out.print("Proporciona valor operando 1:");
+            var operando1 = Integer.parseInt(consola.nextLine().trim());
+            System.out.print("Proporciona valor operando 2:");
+            var operando2 = Integer.parseInt(consola.nextLine().trim());
+            switch (operacion){
+                case 1 ->{
+                    resultado = operando1 + operando2;
+                }
+                case 2 ->{
+                    resultado = operando1 - operando2;
+                }
+                case 3 ->{
+                    resultado = operando1 * operando2;
+                }
+                case 4 ->{
+                    resultado = operando1 / operando2;
+                }
+                default -> {
+                    System.out.println("Operación no válida");
+                    resultado = 0;
+                }
+
+            }
+            System.out.println("Resultado: " + resultado);
+
+        } else if (operacion == 5){
+            System.out.println("Hasta luego");
+        } else {
+            System.out.println("Operación no válida");
+        }
+
     }
 
 }
