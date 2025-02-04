2a8fecaae79c252723ff7a1d19260b6d817b3d1c
javiro76 <javiro76@gmail.com>
2025-02-03 21:11:46 -0500

    cycle while - try and catch

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -4,52 +4,63 @@ public class CalculadoraApp {
 
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        System.out.println("**** Aplicación Calculadora ****");
-        System.out.println("""
-        Selecciona el tipo de operación:
-        1. Suma
-        2. Resta
-        3. Multiplicación
-        4. División
-        5. Salir
-        """);
-        System.out.print("Opción Seleccionada:");
-        var operacion = Integer.parseInt(consola.nextLine().trim());
 
-        int resultado;
+        while (true) {
+            try {
 
-        if( operacion >= 1 && operacion <= 4 ){
-            System.out.print("Proporciona valor operando 1:");
-            var operando1 = Integer.parseInt(consola.nextLine().trim());
-            System.out.print("Proporciona valor operando 2:");
-            var operando2 = Integer.parseInt(consola.nextLine().trim());
-            switch (operacion){
-                case 1 ->{
-                    resultado = operando1 + operando2;
-                }
-                case 2 ->{
-                    resultado = operando1 - operando2;
-                }
-                case 3 ->{
-                    resultado = operando1 * operando2;
-                }
-                case 4 ->{
-                    resultado = operando1 / operando2;
-                }
-                default -> {
+                System.out.println("**** Aplicación Calculadora ****");
+                System.out.println("""
+                        Selecciona el tipo de operación:
+                        1. Suma
+                        2. Resta
+                        3. Multiplicación
+                        4. División
+                        5. Salir
+                        """);
+                System.out.print("Opción Seleccionada:");
+                var operacion = Integer.parseInt(consola.nextLine().trim());
+
+                int resultado;
+
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.print("Proporciona valor operando 1:");
+                    var operando1 = Integer.parseInt(consola.nextLine().trim());
+                    System.out.print("Proporciona valor operando 2:");
+                    var operando2 = Integer.parseInt(consola.nextLine().trim());
+                    switch (operacion) {
+                        case 1 -> {
+                            resultado = operando1 + operando2;
+                        }
+                        case 2 -> {
+                            resultado = operando1 - operando2;
+                        }
+                        case 3 -> {
+                            resultado = operando1 * operando2;
+                        }
+                        case 4 -> {
+                            resultado = operando1 / operando2;
+                        }
+                        default -> {
+                            System.out.println("Operación no válida");
+                            resultado = 0;
+                        }
+
+                    }
+                    System.out.println("Resultado: " + resultado);
+
+                } else if (operacion == 5) {
+                    System.out.println("Hasta luego...");
+                    break;
+                } else {
                     System.out.println("Operación no válida");
-                    resultado = 0;
                 }
+                //Se imprimi salto de linea para separar las operaciones
+                System.out.println();
+            }// fin de try
+            catch (Exception e) {
+                System.out.println("Ocurrió un error!" + e.getMessage());
 
             }
-            System.out.println("Resultado: " + resultado);
-
-        } else if (operacion == 5){
-            System.out.println("Hasta luego");
-        } else {
-            System.out.println("Operación no válida");
-        }
-
+        }// fin de while
     }
-
 }
