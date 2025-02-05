27c2a98186fd9cc0bcf195955e682f49a5947d85
javiro76 <javiro76@gmail.com>
2025-02-04 21:42:51 -0500

    REFACTOR: inplement funtions menu and operationExecute

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -9,44 +9,17 @@ public class CalculadoraApp {
             try {
 
                 System.out.println("**** Aplicación Calculadora ****");
-                System.out.println("""
-                        Selecciona el tipo de operación:
-                        1. Suma
-                        2. Resta
-                        3. Multiplicación
-                        4. División
-                        5. Salir
-                        """);
-                System.out.print("Opción Seleccionada:");
+                //Mostramos el menú de opciones
+                menu();
+
+
                 var operacion = Integer.parseInt(consola.nextLine().trim());
 
-                int resultado;
 
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.print("Proporciona valor operando 1:");
-                    var operando1 = Integer.parseInt(consola.nextLine().trim());
-                    System.out.print("Proporciona valor operando 2:");
-                    var operando2 = Integer.parseInt(consola.nextLine().trim());
-                    switch (operacion) {
-                        case 1 -> {
-                            resultado = operando1 + operando2;
-                        }
-                        case 2 -> {
-                            resultado = operando1 - operando2;
-                        }
-                        case 3 -> {
-                            resultado = operando1 * operando2;
-                        }
-                        case 4 -> {
-                            resultado = operando1 / operando2;
-                        }
-                        default -> {
-                            System.out.println("Operación no válida");
-                            resultado = 0;
-                        }
 
-                    }
-                    System.out.println("Resultado: " + resultado);
+                    ejecutarOperacion(operacion, consola);
+
 
                 } else if (operacion == 5) {
                     System.out.println("Hasta luego...");
@@ -62,5 +35,46 @@ public class CalculadoraApp {
 
             }
         }// fin de while
+
+    }// fin de main
+
+    public static void menu() {
+        System.out.println("""
+                Selecciona el tipo de operación:
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. División
+                5. Salir
+                """);
+        System.out.print("Opción Seleccionada:");
+    }
+
+    private static void ejecutarOperacion(int operacion, Scanner consola) {
+        double resultado;
+        System.out.print("Proporciona valor operando 1:");
+        var operando1 = Double.parseDouble(consola.nextLine().trim());
+        System.out.print("Proporciona valor operando 2:");
+        var operando2 = Double.parseDouble(consola.nextLine().trim());
+        switch (operacion) {
+            case 1 -> {
+                resultado = operando1 + operando2;
+            }
+            case 2 -> {
+                resultado = operando1 - operando2;
+            }
+            case 3 -> {
+                resultado = operando1 * operando2;
+            }
+            case 4 -> {
+                resultado = operando1 / operando2;
+            }
+            default -> {
+                System.out.println("Operación no válida");
+                resultado = 0;
+            }
+
+        }
+        System.out.println("Resultado: " + resultado);
     }
 }
